455667c43a0dc1249e7cc39c7b33b9107639affc
Rohit <90521597+Rohit311098@users.noreply.github.com>
2023-10-01 23:29:11 +0530

    Create 921. Minimum Add to Make Parentheses Valid.java

---
+++ Day 3/921. Minimum Add to Make Parentheses Valid.java
@@ -0,0 +1,27 @@
+ public int minAddToMakeValid(String s) {
+
+        int open=0;
+        int close=0;
+
+        for(char c:s.toCharArray())
+        {
+            if(c=='(')
+            {
+                open++;
+            }
+            else
+            {
+                if(open>0)
+                {
+                    open--;
+                }
+                else
+                {
+                    close++;
+                }
+            }
+        }
+
+        return open+close;
+        
+    }
